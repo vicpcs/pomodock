f6ed46cad891472ee30803734eab725835dc4546
Vic Paz <vic@vicpaz.codes>
2026-01-15 23:35:22 -0600

    include image display in controller

--- examples/service/picSwitchApp.py
+++ examples/service/picSwitchApp.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from inkyController import show_jjk, show_demon_slayer
 
 app = FastAPI()
 
@@ -8,8 +9,10 @@ def hello():
 
 @app.get("/jjk")
 def show_jjk():
+    show_jjk()
     return {"image": "Displaying Jujutsu Kaisen image"}
 
 @app.get("/demon-slayer")
 def show_demon_slayer():
+    show_demon_slayer()
     return {"image": "Displaying Demon Slayer image"}
