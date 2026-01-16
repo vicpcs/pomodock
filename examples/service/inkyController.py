7a044347f3bc190d2601a6732821eb4f69adc133
Vic Paz <vic@vicpaz.codes>
2026-01-15 23:48:59 -0600

    Add example folder root

--- examples/service/inkyController.py
+++ examples/service/inkyController.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from inky.auto import auto
 from PIL import Image
 
-base_dir = Path(__file__).resolve().parent
+EXAMPLE_ROOT = Path(__file__).resolve().parent.parent
 inky = auto(ask_user=True, verbose=True)
 
 def show_image(resized_image):
@@ -12,11 +12,11 @@ def show_image(resized_image):
         inky.set_image(resized_image)
 
 def draw_jjk_image():
-    image = Image.open(base_dir / "images" / "jjk.jpg")
+    image = Image.open(EXAMPLE_ROOT / "images" / "jjk.jpg")
     resized_image = image.resize(inky.resolution)
     show_image(resized_image)
 
 def draw_demon_slayer_image():
-    image = Image.open(base_dir / "images" / "demonSlayer.jpg")
+    image = Image.open(EXAMPLE_ROOT / "images" / "demonSlayer.jpg")
     resized_image = image.resize(inky.resolution)
     show_image(resized_image)
