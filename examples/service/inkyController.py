5a047cb98c1c09493ca8223f068da48b71e3c041
Vic Paz <vic@vicpaz.codes>
2026-01-19 22:52:01 -0600

    Remove hardcoded funcs

--- examples/service/inkyController.py
+++ examples/service/inkyController.py
@@ -9,32 +9,12 @@ def show_image(resized_image):
     print("Displaying image on Inky...")
     print(resized_image)
     try:
-        inky.set_image(resized_image, saturation=0.5)
+        inky.set_image(resized_image, saturation=0)
     except TypeError:
         inky.set_image(resized_image)
     inky.show()
 
 def draw_image(image_path):
     image = Image.open(image_path)
-    resized_image = image.resize(inky.resolution)
-    show_image(resized_image)
-
-def draw_jjk_image():
-    image = Image.open(EXAMPLE_DIR_ROOT / "images" / "jjk.png")
-    resized_image = image.resize(inky.resolution)
-    show_image(resized_image)
-
-def draw_demon_slayer_image():
-    image = Image.open(EXAMPLE_DIR_ROOT / "images" / "demonSlayer.jpg")
-    resized_image = image.resize(inky.resolution)
-    show_image(resized_image)
-
-def draw_mha_image():
-    image = Image.open(EXAMPLE_DIR_ROOT / "images" / "mha.jpg")
-    resized_image = image.resize(inky.resolution)
-    show_image(resized_image)
-
-def draw_fire_force_image():
-    image = Image.open(EXAMPLE_DIR_ROOT / "images" / "fireForce.jpg")
     resized_image = image.resize(inky.resolution)
     show_image(resized_image)
